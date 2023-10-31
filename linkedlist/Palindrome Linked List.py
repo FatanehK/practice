6fee2c14c1ec66023094726a6af635eea1149816
fataneh <fkarandish1@gmail.com>
2023-10-31 09:40:26 -0700

    differnt approach

--- linkedlist/Palindrome Linked List.py
+++ linkedlist/Palindrome Linked List.py
@@ -1,3 +1,8 @@
+class ListNode:
+    def __init__(self,val,next =None):
+        self.val = val
+        self.next = next
+
 def reverse(head):
     curr = head
     ans= None
@@ -29,3 +34,53 @@ def Palindrome_Linked_List(head):
         second = second.next
     return True
 
+
+
+def Palindrome_Linked_List(head):
+    making a data structure
+    nums=[]
+    while head:
+        nums.append(head.val)
+        head = head.next
+    l=0
+    r = len(nums)-1
+    while l<= r:
+        if l.val != r.val:
+            return False
+        l= l.next
+        r= r.next
+    return True
+
+
+def Palindrome_Linked_List(head):
+    # find the middle 
+    slow = head
+    fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    # reverse the second half
+    prev= None
+    curr= slow
+    while curr:
+        nxt_node = curr.next
+        curr.next= prev
+        prev = curr
+        curr = nxt_node
+
+    # compare 
+    first = head
+    second = prev
+    while second:
+        if first.val != second.val:
+            return False
+        first= first.next
+        second = second.next
+    return True
+
+
+ll= ListNode("1")
+ll.next = ListNode("2")
+ll.next.next = ListNode("5")
+ll.next.next.next = ListNode("1")
+print(Palindrome_Linked_List(ll))
