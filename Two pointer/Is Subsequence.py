2d18c13f20519d24e0380837426ad18b75ab1282
fataneh <fkarandish1@gmail.com>
2023-11-11 14:26:59 -0800

    Is Subsequence different approach

--- Two pointer/Is Subsequence.py
+++ Two pointer/Is Subsequence.py
@@ -4,17 +4,28 @@ def isSubsequence(s, t):
         :type t: str
         :rtype: bool
     """
-    i = len(s)-1
-    j = len(t)-1
-    if len(s)== 0:
-        return True
-    while i < j:
-        if s[i] == t[j]:
-            i-=1
-            j-=1
-        elif s[i] != t[j]:
-            j-=1
-    return i == len(s)
+    # i = len(s)-1
+    # j = len(t)-1
+    # if len(s)== 0:
+    #     return True
+    # while i < j:
+    #     if s[i] == t[j]:
+    #         i-=1
+    #         j-=1
+    #     elif s[i] != t[j]:
+    #         j-=1
+    # return i == len(s)
+
+
+    if len(s)> len(t):
+        return False
+    i =0
+    j=0
+    while i < len(s)and j< len(t):
+        if t[j]== s[i]:
+            i+=1
+        j+=1
+    return True if i == len(s) else False
 
 
 print(isSubsequence("abc", "ahbgdc"))
