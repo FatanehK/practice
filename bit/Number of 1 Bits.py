6f17201cf387932b9e3f3ad55b0c2c437fee0e39
fataneh <fkarandish1@gmail.com>
2023-11-13 10:56:48 -0800

    count the number of 1s

--- bit/Number of 1 Bits.py
+++ bit/Number of 1 Bits.py
@@ -14,12 +14,9 @@ Binary representation of 13 is 1101 and has 3 set bits
 
 def countSetBits(n):
     count = 0
-    while (n):
+    while n:
         count += n & 1
-        n >>= 1
+        n >>= 1# equivalnt to div number by 2
     return count
 
-
-# Program to test function countSetBits */
-i = 9
 print(countSetBits(3))
