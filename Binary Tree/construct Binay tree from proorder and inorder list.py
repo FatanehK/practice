fe7647bc2f0322065b2e9c7e06c45a2aa5c11eb4
fataneh <fkarandish1@gmail.com>
2023-11-26 09:59:01 -0800

    Build Tree

--- Binary Tree/construct Binay tree from proorder and inorder list.py
+++ Binary Tree/construct Binay tree from proorder and inorder list.py
@@ -30,6 +30,7 @@ class TreeNode:
         mid = inorder.index(preorder[0])
         root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
         root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+        return root
 
 tree = TreeNode()
 print(tree.buildTree( [3,9,20,15,7], [9,3,15,20,7]))
