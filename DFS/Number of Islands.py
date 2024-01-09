410f9b6f1ebb88906363ad988fdca7d87b77f08a
fataneh <fkarandish1@gmail.com>
2024-01-08 23:52:13 -0800

    update

--- DFS/Number of Islands.py
+++ DFS/Number of Islands.py
@@ -28,30 +28,34 @@ n == grid[i].length
 1 <= m, n <= 300
 grid[i][j] is '0' or '1'
 """
+
+
 def numIslands(grid):
-    def dfs(i, j):
+    def dfs(grid,i, j):
         if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != "1":
             return False
         grid[i][j] = "*"
-        dfs(i + 1, j)
-        dfs(i - 1, j)
-        dfs(i, j + 1)
-        dfs(i, j - 1)
+        dfs(grid, i + 1, j)
+        dfs(grid, i - 1, j)
+        dfs(grid, i, j + 1)
+        dfs(grid, i, j - 1)
 
     count = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == "1":
-                dfs(i, j)
+                dfs(grid, i, j)
                 count += 1
     return count
 
 
-print(numIslands(
-    [
-        ["1", "1", "1", "1", "0"],
-        ["1", "1", "0", "1", "0"],
-        ["1", "1", "0", "0", "0"],
-        ["0", "0", "0", "0", "0"],
-    ]
-))
+print(
+    numIslands(
+        [
+            ["1", "1", "1", "1", "0"],
+            ["1", "1", "0", "1", "0"],
+            ["1", "1", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"],
+        ]
+    )
+)
