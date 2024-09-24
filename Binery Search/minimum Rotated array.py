619026ca132baabc6ba383860ad0d70a80f1b01d
fataneh <fkarandish1@gmail.com>
2024-09-24 16:32:05 -0700

    update code

--- Binery Search/minimum Rotated array.py
+++ Binery Search/minimum Rotated array.py
@@ -1,14 +1,15 @@
 def min_rotated_sortedarry(arr):
-    low =0
-    high= len(arr)-1
+    low = 0
+    high = len(arr) - 1
 
-    while low< high:
-        mid = low + (high- low)//2
+    while low < high:
+        mid = low + (high - low) // 2
 
         if arr[mid] < arr[high]:
             high = mid
         else:
-            low = mid+1
+            low = mid + 1
     return arr[low]
-    
-print(min_rotated_sortedarry([3,4,5,1,2]))
+
+
+print(min_rotated_sortedarry([3, 4, 5, 1]))
