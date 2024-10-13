53e366f7a7a52dd5fe63d291d8d054b592896423
fataneh <fkarandish1@gmail.com>
2024-10-13 14:44:14 -0700

    longest valid Parenthesis

--- stack/Longest Valid Parentheses.py
+++ stack/Longest Valid Parentheses.py
@@ -1,14 +1,16 @@
-def longest_valid_parentheses(s):
-    max_len = 0
-    stack = [-1]
-    for i, char in enumerate(s):
-        if char == "(":
+def longest_valid_paranthesis(s):
+    stack =[-1]
+    mx_len=0
+    for i , char in enumerate(s):
+        if char =="(":
             stack.append(i)
-        else:
+        elif char == ")":
             stack.pop()
             if not stack:
                 stack.append(i)
             else:
-                max_len = max(max_len, i - stack[-1])
-    return max_len
-print(longest_valid_parentheses("()"))
+                mx_len = max(mx_len,i-stack[-1])
+
+    return mx_len
+
+print(longest_valid_paranthesis("(()()())"))
